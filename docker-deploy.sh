190b08be172e3920888846416e38c6b6a3eb4ca9
khakhux <67237104+khakhux@users.noreply.github.com>
2021-11-25 12:56:55 +0100

    parametizo url de git

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
+
+branch=2021-11-axia
+git_user=khakhux
 tag=latest
-git clone --branch 2021-11-axia --single-branch https://github.com/academiaonline-org/dockercoins
+
+git clone --branch ${branch} --single-branch https://github.com/${git_user}/dockercoins
+
+cd dockercoin
+
 for app in hasher redis rng webui worker
 do
 docker build -t ${app}:${tag} ${app}
 done
+
 for app in hasher redis rng
 do 
 docker network create ${app}
 done
+
 cmd=redis-server
 entrypoint=/usr/local/bin/docker-entrypoint.sh
 image=redis
@@ -20,4 +29,5 @@ volume=redis
 volume_path=/data/
 volume_ops=rw
 workdir=/data/
+
 docker run -d --entrypoint ${entrypoint} --name ${name} --network ${network} --read-only --restart ${restart} -u ${user} -v ${volume}:${volume_path}:${volume_ops} -w ${workdir} ${image}:${tag} ${cmd}
